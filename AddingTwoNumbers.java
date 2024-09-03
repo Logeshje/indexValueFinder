bf9f0123787ba8864f6f6edd6473f82606981509
Logeshje <logeshkannanje@gmail.com>
2024-09-03 11:25:40 +0530

    AddingTwoNumbers.java

---
+++ AddingTwoNumbers.java
@@ -0,0 +1,21 @@
+package String;
+
+import java.util.Scanner;
+
+public class AddingTwoNumbers{
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the 1st digit: ");
+		int x = scan.nextInt();
+		System.out.println("Enter the 2nd digit: ");
+		int y = scan.nextInt();
+		int z = x+y;
+		System.out.println(z);
+		
+		
+		
+		
+	}
+	
+}
